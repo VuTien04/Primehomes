d7a3601a552ab185993ef10cdc8da81eba093d13
VuTien04 <dovutien2004@gmail.com>
2025-11-25 18:06:38 +0700

    Update: be-customer

--- spring-boot-api-non-jwt/src/main/java/com/javaweb/CustomExceptions/InvalidDataException.java
+++
@@ -1,9 +0,0 @@
-package com.javaweb.CustomExceptions;
-
-public class InvalidDataException extends RuntimeException {
-
-	public InvalidDataException(String message) {
-		super(message);
-	}
-
-}
--- spring-boot-api-non-jwt/src/main/java/com/javaweb/Utils/DataUtil.java
+++
@@ -1,8 +0,0 @@
-package com.javaweb.Utils;
-
-public class DataUtil {
-	public static boolean check(Object string) {
-		if(string != null && !string.equals("")) return true;
-		return false;
-	}
-}
